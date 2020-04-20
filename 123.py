6c98749c03ba1ece9dc3e47e32257c329a1f04be
Sunny Choi <yukhengchoi@gmail.com>
2020-04-20 22:07:44 +0800

    Add New File

---
+++ 123.py
@@ -0,0 +1,7 @@
+name = input('Please enter your full name：')
+
+height = input('Please enter your height(in cm):')
+weight = input('Please enter your weight(in kg):')
+print('Your height is:', height, 'cm')
+print('Your weight is:', weight, 'kg')
+print('Hi！肥肥豬', name)
